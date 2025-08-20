be9a9555487d2393b5c631fe30ed3ffb48811aa7
Chirag J <chiragjattavar09@gmail.com>
2025-08-20 21:05:27 +0530

    Update Showroom.java

--- Showroom.java
+++ Showroom.java
@@ -3,7 +3,7 @@ package cjchiraglab2;
 public class Showroom {
 	public static void main(String[] args) {
         Car c1 = new Car("Porsche", "911", 45000000, 13.6);
-        Car c2 = new Car("Honda", "Civic", 170000, 24.7, "John");
+        Car c2 = new Car("Honda", "Civic", 170000, 24.7, "Namith");
         Car c3 = new Car("Suzuki", "supra", 788000, 19.9);
 
         c1.displaydetails();
